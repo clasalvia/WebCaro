f1f15602fd6853d6d5d5d17b1fb139a377921752
clasalvia <claslavia@gmail.com>
2022-05-23 07:34:08 -0700

    a

--- mainDOM.js
+++ mainDOM.js
@@ -4,3 +4,10 @@ console.log("soy externo")
 
 let parrafo1 = document.getElementById("pConId")
 console.log(parrafo1)
+
+let parrafo2 = document.getElementsByClassName("pConClass")
+console.log(parrafo2)
+
+let parrafo3 = document.getElementsByTagName("p")
+console.log(parrafo3)
+
