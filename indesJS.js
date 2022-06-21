6cfb9934854bb104e47b7af8f183cc9d00b490d6
clasalvia <clasalvia@itba.edu.ar>
2022-06-21 16:33:29 -0300

    a

--- indesJS.js
+++ indesJS.js
@@ -1,22 +1,22 @@
-
 //SECCION CLASES
 // Hide all elements with class="containerTab", except for the one that matches the clickable grid column
 function openTab(tabName) {
-    let i, x;
-    x = document.getElementsByClassName("containerTab");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
+  let i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-
+  document.getElementById(tabName).style.display = "block";
+}
 
 //SECCION BOTON SCROLL TO THE TOP
-  //Get the button:
+//Get the button:
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
